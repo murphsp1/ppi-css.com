dd55b63fb817ec0cf0d597ce697f847dd868957d
Sean Murphy <murphsp1@gmail.com>
2013-10-22 19:51:49 -0400

    painfully renamed myapp to css0

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -152,7 +152,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'django_extensions',
-    'myproject.myapp',
+    'myproject.css0',
     'south'
 )
 
