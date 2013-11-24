d9c105773f7c292c081fb4d8359e6ef2e6c09e96
Sean Murphy <murphsp1@gmail.com>
2013-11-24 14:33:29 -0500

    trying to remove those deleted files

--- myproject/myproject/css0/forms.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django import forms
-
-class DocumentForm(forms.Form):
-    docfile = forms.FileField(
-        label='Select a PDB file to upload',
-        help_text='(max. 42MB)'
-    )
